8b1b41c7f0ea24188bd2ddc48bed8aa9d569691b
yangrui123456 <15678993019@163.com>
2018-12-07 17:40:03 +0800

    commit

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption" v-if="showSwiper">
+      <swiper-slide v-for="item of list" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" />
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  props: {
+    list: Array
+  },
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      }，
+	  list:[{
+                id:'0001',
+                imgUrl:'http://img1.qunarzz.com/piao/fusion/1809/c6/2467595fffc3b302.jpg_750x200_cca13d51.jpg'
+            },
+              {  id:'0002',
+                imgUrl:'http://img1.qunarzz.com/piao/fusion/1811/1b/4143c7e0bfe78d02.jpg_750x200_5a220f6e.jpg'
+            },
+             {  id:'0003',
+                imgUrl:'http://img1.qunarzz.com/piao/fusion/1811/ce/77e115fea8f99102.jpg_750x200_a2d0fcd2.jpg'
+            },
+            {  id:'0004',
+                imgUrl:'http://img1.qunarzz.com/piao/fusion/1809/ae/7518c2fbc1415202.jpg_750x200_09bbe391.jpg'
+            }]
+    }
+  },
+  computed: {
+    showSwiper () {
+      return this.list.length
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 31.25%
+    background: #eee
+    .swiper-img
+      width: 100%
+</style>
